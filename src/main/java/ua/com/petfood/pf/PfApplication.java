ce48603d51d9c2c19cfbe04e0ab2ae21e469f131
Den <den.lutsenko@gmail.com>
2021-01-13 13:32:39 +0200

    init commit

---
+++ src/main/java/ua/com/petfood/pf/PfApplication.java
@@ -0,0 +1,13 @@
+package ua.com.petfood.pf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/com/petfood/pf/PfApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.petfood.pf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
