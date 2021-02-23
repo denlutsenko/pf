d8035eacb6f1c37e3f70c3cabc42452d648ccfda
khmild01@kits.kfplc.com <khmild01@kits.kfplc.com>
2021-02-23 18:01:44 +0200

    [PF-54] Refactoring

---
+++ src/main/java/ua/com/petfood/pf/web/CatalogController.java
@@ -0,0 +1,27 @@
+package ua.com.petfood.pf.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ua.com.petfood.pf.service.SKUItemService;
+
+@RestController
+@RequestMapping(value = "/anon")
+public class CatalogController {
+
+    private SKUItemService skuItemService;
+
+    @Autowired
+    public CatalogController(SKUItemService skuItemService) {
+        this.skuItemService = skuItemService;
+    }
+
+    @GetMapping(value = "/items/get/{id}")
+    ResponseEntity getLineItemsForAnimalCategoryById(@PathVariable(name = "id") Long animalCategoryId) {
+        return ResponseEntity.ok(skuItemService.getSKUItemsByAnimalCategoryID(animalCategoryId));
+    }
+
+}
--- src/main/java/ua/com/petfood/pf/web/QuestionnaireController.java
+++ src/main/java/ua/com/petfood/pf/web/QuestionnaireController.java
@@ -3,9 +3,11 @@ package ua.com.petfood.pf.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ua.com.petfood.pf.model.SKUItem;
 import ua.com.petfood.pf.model.dto.QuestionnaireDto;
 import ua.com.petfood.pf.service.QuestionnaireService;
-import ua.com.petfood.pf.service.SKUItemService;
+
+import java.util.List;
 
 @RestController
 //@CrossOrigin
@@ -13,22 +15,15 @@ import ua.com.petfood.pf.service.SKUItemService;
 public class QuestionnaireController {
 
     private QuestionnaireService questionnaireService;
-    private SKUItemService skuItemService;
 
     @Autowired
-    public QuestionnaireController(QuestionnaireService questionnaireService, SKUItemService skuItemService) {
+    public QuestionnaireController(QuestionnaireService questionnaireService) {
         this.questionnaireService = questionnaireService;
-        this.skuItemService = skuItemService;
     }
 
     @GetMapping(value = "/questions/catalogs/all")
     ResponseEntity getCatalogsForQuestionnaire() {
         return ResponseEntity.ok(questionnaireService.getAllCatalogsForQuestionnaire());
-    }
-
-    @GetMapping(value = "/items/get/{id}")
-    ResponseEntity getLineItemsForAnimalCategoryById(@PathVariable(name = "id") Long animalCategoryId) {
-        return ResponseEntity.ok(skuItemService.getSKUItemsByAnimalCategoryID(animalCategoryId));
     }
 
     @PostMapping(value = "/meal/recommend")
