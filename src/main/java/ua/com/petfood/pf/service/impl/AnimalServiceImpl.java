9a7df193706257c70bd0e974bf6405fe1bf1c507
lutsed01 <lutsed01@kits.kfplc.com>
2021-03-03 16:16:56 +0200

    POST Animal call

--- src/main/java/ua/com/petfood/pf/service/impl/AnimalServiceImpl.java
+++ src/main/java/ua/com/petfood/pf/service/impl/AnimalServiceImpl.java
@@ -9,6 +9,7 @@ import ua.com.petfood.pf.model.AnimalCategory;
 import ua.com.petfood.pf.model.User;
 import ua.com.petfood.pf.model.dto.QuestionnaireDTO;
 import ua.com.petfood.pf.repository.AnimalRepository;
+import ua.com.petfood.pf.service.AnimalCategoryService;
 import ua.com.petfood.pf.service.AnimalService;
 import ua.com.petfood.pf.service.UserService;
 
@@ -16,13 +17,15 @@ import ua.com.petfood.pf.service.UserService;
 public class AnimalServiceImpl implements AnimalService {
 
     private final AnimalRepository animalRepository;
+    private final AnimalCategoryService animalCategoryService;
     private final UserHelper userHelper;
     private final UserService userService;
 
     @Autowired
     private AnimalServiceImpl(final AnimalRepository animalRepository, final UserHelper userHelper,
-            final UserService userService) {
+            final AnimalCategoryService animalCategoryService, final UserService userService) {
         this.animalRepository = animalRepository;
+        this.animalCategoryService = animalCategoryService;
         this.userHelper = userHelper;
         this.userService = userService;
     }
@@ -33,10 +36,17 @@ public class AnimalServiceImpl implements AnimalService {
         String email = userHelper.getUserEmailFromToken(bearerToken);
         User user = userService.findByUsername(email);
 
-        Animal animal =  new Animal(animalCategory, user, questionnaireDto.getName(),
+        Animal animal = new Animal(animalCategory, user, questionnaireDto.getName(),
                 questionnaireDto.getSubCategoryName(), questionnaireDto.getAge(), questionnaireDto.getAdultDogSize());
 
         return saveAnimal(animal);
+    }
+
+    @Override
+    public Animal createAndSaveAnimal(final QuestionnaireDTO questDto, final String bearerToken) {
+        AnimalCategory animalCategory = animalCategoryService.getAnimalCategoryById(questDto.getPetCategoryId());
+
+        return createAndSaveAnimal(questDto, animalCategory, bearerToken);
     }
 
     private Animal saveAnimal(Animal animal) {
